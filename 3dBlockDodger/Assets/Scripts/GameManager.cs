570ec664c794f6378c0807d496e60191c9b147fa
FishSt1ck <66172388+FishSt1ck-git@users.noreply.github.com>
2020-08-08 15:24:11 +0200

    tweaked some settings

--- 3dBlockDodger/Assets/Scripts/GameManager.cs
+++ 3dBlockDodger/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public float restartDelay = 1;
-
+    
     private bool gameHasEnded = false;
 
     public void EndGame()
